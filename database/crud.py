5ccdc1e28e277394488aab38f3b72ab5b4201c49
notttsozu <sonukumarsuman@Sonus-MacBook-Pro.local>
2026-07-30 09:25:26 +0530

    feat(database): add email lookup functionality in crud layer

--- database/crud.py
+++ database/crud.py
@@ -11,6 +11,10 @@ def get_user(db: Session, user_id: int):
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
 
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(
         username=user.username,
